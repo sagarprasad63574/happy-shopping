5a4a1263eddc4e8240306e06bc9049657980a3bf
Sagar Prasad <sagarprasad63574@gmail.com>
2023-05-22 19:59:45 -0700

    worked on database

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/** Express app for jobly. */
+/** Express app for happy_shopping. */
 
 const express = require("express");
 const cors = require("cors");
@@ -17,9 +17,9 @@ const morgan = require("morgan");
 
 const app = express();
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
 // app.use(authenticateJWT);
 
 // app.use("/auth", authRoutes);
@@ -28,20 +28,20 @@ const app = express();
 // app.use("/jobs", jobsRoutes);
 
 
-// /** Handle 404 errors -- this matches everything */
-// app.use(function (req, res, next) {
-//     return next(new NotFoundError());
-// });
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+});
 
-// /** Generic error handler; anything unhandled goes here. */
-// app.use(function (err, req, res, next) {
-//     if (process.env.NODE_ENV !== "test") console.error(err.stack);
-//     const status = err.status || 500;
-//     const message = err.message;
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
 
-//     return res.status(status).json({
-//         error: { message, status },
-//     });
-// });
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
 
 module.exports = app;
